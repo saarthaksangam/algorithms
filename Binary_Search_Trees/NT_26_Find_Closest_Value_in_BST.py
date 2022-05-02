e8f67f80e399d4462d2dfb5cc3ffaa77a194f02c
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2022-05-01 20:56:34 -0400

    Update NT_26_Find_Closest_Value_in_BST.py

--- Binary_Search_Trees/NT_26_Find_Closest_Value_in_BST.py
+++ Binary_Search_Trees/NT_26_Find_Closest_Value_in_BST.py
@@ -11,17 +11,6 @@ Date: 05/01/2022
 
 
 def findClosestValueInBst(tree, target):
-    """
-    The findClosestValueInBst function finds the closest value to a target value in a BST.
-       The function takes in two parameters: tree and target, both of which are integers.
-       The function returns an integer.
-
-    :param tree: Keep track of the tree that is being searched
-    :param target: Store the value of the node that is being searched for
-    :return: The closest value to the target
-    :doc-author: Trelent
-    """
-    # Write your code here.
     return findClosestValueInBSTHelperRecursive(tree, target, float('inf'))
 
 
@@ -33,19 +22,7 @@ Average Space Complexity: O(1)
 Worst Time Complexity: O(n)
 Worst Space Complexity: O(n)
 '''
-
-
 def findClosestValueInBSTHelperRecursive(tree, target, closest):
-    """
-    The findClosestValueInBSTHelperRecursive function takes in a BST and a target value. It then traverses the tree
-    recursively and returns the closest value to that target
-
-    :param tree: Keep track of the current node
-    :param target: Store the value of the node that is being searched for
-    :param closest: Keep track of the closest value so far
-    :return: The closest value to the target
-    :doc-author: Trelent
-    """
     if tree is None:
         return closest
     if abs(target - closest) > abs(target - tree.value):
@@ -66,20 +43,7 @@ Average Space Complexity: O(1)
 Worst Time Complexity: O(n)
 Worst Space Complexity: O(1)
 '''
-
-
 def findClosestValueInBSTHelperIterative(tree, target, closest=float('inf')):
-    """
-    The findClosestValueInBSTHelperIterative function takes in a BST and a target value,
-    and returns the closest value to that target value contained in the BST. The function
-    uses an iterative approach to find this closest value.
-
-    :param tree: Keep track of the current node
-    :param target: Store the value of the node that is being searched for
-    :param closest=float('inf'): Keep track of the closest value so far
-    :return: The closest value in the bst
-    :doc-author: Trelent
-    """
     currentNode = tree
     while currentNode is not None:
         if abs(target - closest) > abs(target - currentNode.value):
