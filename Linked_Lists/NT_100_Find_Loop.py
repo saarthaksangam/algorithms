ba38497e02c53d08b994d8c82b0383d27e8a014f
Saarthak Sangamnerkar <saarthaksangamnerkar@gmail.com>
2022-05-08 11:07:26 -0400

    adding NT_100_Find_Loop.py

---
+++ Linked_Lists/NT_100_Find_Loop.py
@@ -0,0 +1,34 @@
+"""
+Problem: Find Loop
+Problem Statement: https://www.algoexpert.io/questions/Find%20Loop
+NTID: NT-100
+Category: Linked Lists
+Difficulty: Hard
+Sprint: 2205.01
+Engineer: Saarthak Sangamnerkar
+Date: 05/08/2022
+"""
+
+
+# O(n) time | O(1) space
+def findLoop(head):
+    """
+    The findLoop function finds the first node of a loop in a linked list.
+       It takes as input the head of a linked list and returns the first node
+       in the loop.
+
+    :param head: Keep track of the current node
+    :return: The node at which the loop starts
+    :doc-author: Trelent
+    """
+    slow = head.next  # slow pointer
+    fast = head.next.next  # fast pointer - skips 1 node
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next.next
+    # when fast and slow meet
+    slow = head
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
+    return slow
